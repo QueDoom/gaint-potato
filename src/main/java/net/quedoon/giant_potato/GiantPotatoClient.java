e9a6105ade5f6d4e292ce9b9d801bbb09512534e
Q <norton@totl.se>
2026-03-01 09:09:30 +0100

    Main Classes

---
+++ src/main/java/net/quedoon/giant_potato/GiantPotatoClient.java
@@ -0,0 +1,10 @@
+package net.quedoon.giant_potato;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class GiantPotatoClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
