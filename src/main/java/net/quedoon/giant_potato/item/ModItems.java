700ae2da1d841ae769c1a4b0511f5e8d96deb5d4
Q <norton@totl.se>
2026-03-01 09:07:38 +0100

    Added ModItems class, tiller item

---
+++ src/main/java/net/quedoon/giant_potato/item/ModItems.java
@@ -0,0 +1,21 @@
+package net.quedoon.giant_potato.item;
+
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.quedoon.giant_potato.GiantPotato;
+import net.quedoon.giant_potato.item.custom.TillerItem;
+
+public class ModItems {
+    public static Item TILLER = registerItem("tiller", new TillerItem(new Item.Settings().maxCount(1).maxDamage(500)));
+
+
+    private static Item registerItem(String name, Item item) {
+        return Registry.register(Registries.ITEM, Identifier.of(GiantPotato.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        GiantPotato.LOGGER.info("Registering Mod Items for " + GiantPotato.MOD_ID);
+    }
+}
