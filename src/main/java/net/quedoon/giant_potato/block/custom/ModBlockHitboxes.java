e6863e8d83dd27bf8398b262e7f95388d97a63f5
potat <norton@totl.se>
2026-03-06 18:18:10 +0100

    Fixed Mash Bowl Hitbox

--- src/main/java/net/quedoon/giant_potato/block/custom/ModBlockHitboxes.java
+++ src/main/java/net/quedoon/giant_potato/block/custom/ModBlockHitboxes.java
@@ -8,11 +8,11 @@ import net.minecraft.util.shape.VoxelShapes;
 public class ModBlockHitboxes {
     public static VoxelShape getMashBowlHitbox() {
         return VoxelShapes.union(
-                Block.createCuboidShape(0, 0, 0, 16, 2, 16),
-                Block.createCuboidShape(0, 2, 0, 2, 8, 16),
-                Block.createCuboidShape(14, 2, 0, 16, 8, 2),
-                Block.createCuboidShape(2, 2, 0, 14, 8, 2),
-                Block.createCuboidShape(2, 2, 14, 14, 8, 16)
+                  Block.createCuboidShape(0, 0, 0, 16, 8, 16)//,
+//                Block.createCuboidShape(0, 2, 0, 2, 8, 16),
+//                Block.createCuboidShape(14, 2, 0, 16, 8, 2),
+//                Block.createCuboidShape(2, 2, 0, 14, 8, 2),
+//                Block.createCuboidShape(2, 2, 14, 14, 8, 16)
         );
     }
 }
