fd653a366ee7694a6c12aed5a9093d469e9f5284
potat <norton@totl.se>
2026-03-06 23:05:42 +0100

    Fixed The mash bowl 50% done

--- src/main/java/net/quedoon/giant_potato/block/custom/MashBowlBlock.java
+++ src/main/java/net/quedoon/giant_potato/block/custom/MashBowlBlock.java
@@ -1,10 +1,14 @@
 package net.quedoon.giant_potato.block.custom;
 
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.ShapeContext;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.util.ActionResult;
@@ -44,14 +48,23 @@ public class MashBowlBlock extends Block {
 
     @Override
     public void onLandedUpon(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance) {
-        super.onLandedUpon(world, state, pos, entity, fallDistance);
-        System.out.println("landed, fallDistance: " + fallDistance + ", entity: " + entity.getClass().getSimpleName());
-        double random = Math.random();
-        if (world.isClient()) return;
-
-        if (fallDistance > 0.75F && entity instanceof PlayerEntity && random > .9) {
-            world.setBlockState(pos, state.cycle(STAGE), 4);
+        System.out.println("landed");
+        if (!world.isClient()) {
+            if(entity instanceof PlayerEntity player) {
+                spawnBreakParticles(world, player, BlockPos.ofFloored(pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5), Blocks.SPONGE.getDefaultState());
+                player.playSoundToPlayer(SoundEvents.ENTITY_SLIME_SQUISH_SMALL, SoundCategory.BLOCKS, 1, 1);
+                double random = Math.random();
+                System.out.println("landed_server, " + random);
+                if (fallDistance > 0.5F && random > .9) {
+                    spawnBreakParticles(world, player, BlockPos.ofFloored(pos.getX() + 0.4, pos.getY() + 0.4, pos.getZ() + 0.4), Blocks.SPONGE.getDefaultState());
+                    spawnBreakParticles(world, player, BlockPos.ofFloored(pos.getX() + 0.6, pos.getY() + 0.6, pos.getZ() + 0.6), Blocks.SPONGE.getDefaultState());
+                    player.playSoundToPlayer(SoundEvents.ENTITY_SLIME_SQUISH, SoundCategory.BLOCKS, 1, 1);
+                    System.out.println("landed_server_height, " + fallDistance);
+                    world.setBlockState(pos, state.cycle(STAGE), 4);
+                }
+            }
         }
+        super.onLandedUpon(world, state, pos, entity, fallDistance);
     }
 
 }
